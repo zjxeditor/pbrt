e0159cbe231df8f42173c6d44cc077d8f50f7fa9
jxzhang <zjxworld@outlook.com>
2017-11-21 11:18:40 +0800

    Finish primitives.

--- src/core/primitives.h
+++ src/core/primitives.h
@@ -33,9 +33,75 @@ public:
                                             bool allowMultipleLobes) const = 0;
 };
 
+// GeometricPrimitive Declarations
+class GeometricPrimitive : public Primitive {
+public:
+    // GeometricPrimitive Public Methods
+    GeometricPrimitive(const std::shared_ptr<Shape> &shape,
+                       const std::shared_ptr<Material> &material,
+                       const std::shared_ptr<AreaLight> &areaLight,
+                       const MediumInterface &mediumInterface)
+            : shape(shape),
+              material(material),
+              areaLight(areaLight),
+              mediumInterface(mediumInterface) {}
+
+    virtual Bounds3f WorldBound() const;
+    virtual bool Intersect(const Ray &r, SurfaceInteraction *isect) const;
+    virtual bool IntersectP(const Ray &r) const;
+    const AreaLight *GetAreaLight() const;
+    const Material *GetMaterial() const;
+    void ComputeScatteringFunctions(SurfaceInteraction *isect,
+                                    MemoryArena &arena, TransportMode mode,
+                                    bool allowMultipleLobes) const;
+
+private:
+    // GeometricPrimitive Private Data
+    std::shared_ptr<Shape> shape;
+    std::shared_ptr<Material> material;
+    std::shared_ptr<AreaLight> areaLight;
+    MediumInterface mediumInterface;
+};
+
+// TransformedPrimitive Declarations
+class TransformedPrimitive : public Primitive {
+public:
+    // TransformedPrimitive Public Methods
+    TransformedPrimitive(std::shared_ptr<Primitive> &primitive,
+                         const AnimatedTransform &PrimitiveToWorld)
+            : primitive(primitive), PrimitiveToWorld(PrimitiveToWorld) {}
+    bool Intersect(const Ray &r, SurfaceInteraction *in) const;
+    bool IntersectP(const Ray &r) const;
+    const AreaLight *GetAreaLight() const { return nullptr; }
+    const Material *GetMaterial() const { return nullptr; }
+    void ComputeScatteringFunctions(SurfaceInteraction *isect,
+                                    MemoryArena &arena, TransportMode mode,
+                                    bool allowMultipleLobes) const {
+        LOG(FATAL) <<
+                   "TransformedPrimitive::ComputeScatteringFunctions() shouldn't be "
+                           "called";
+    }
+    Bounds3f WorldBound() const {
+        return PrimitiveToWorld.MotionBounds(primitive->WorldBound());
+    }
+
+private:
+    // TransformedPrimitive Private Data
+    std::shared_ptr<Primitive> primitive;
+    const AnimatedTransform PrimitiveToWorld;
+};
+
+// Aggregate Declarations. Accelerator should subclass this class.
+class Aggregate : public Primitive {
+public:
+    // Aggregate Public Methods
+    const AreaLight *GetAreaLight() const;
+    const Material *GetMaterial() const;
+    void ComputeScatteringFunctions(SurfaceInteraction *isect,
+                                    MemoryArena &arena, TransportMode mode,
+                                    bool allowMultipleLobes) const;
+};
+
 }   // namespace pbrt
-
-
-
 
 #endif //PBRT_CORE_PRIMITIVES_H
